ef5016295e91de8c05cfeb6a8a806f4cc9cd3398
Chris Oliver <excid3@gmail.com>
2016-11-04 12:24:16 -0500

    Clean up some syntax

--- app/assets/javascripts/uploads.js
+++ app/assets/javascripts/uploads.js
@@ -3,18 +3,18 @@ $(document).on("turbolinks:load", function() {
   $("[type=file]").fileupload({
     add: function(e, data) {
       console.log("add", data);
-      data.progressBar = $('<div class="progress" style="width: 300px"><div class="progress-bar"></div></div>').insertAfter("body")
+      data.progressBar = $('<div class="progress" style="width: 300px"><div class="progress-bar"></div></div>').insertAfter("body");
       var options = {
         extension: data.files[0].name.match(/(\.\w+)?$/)[0], // set the file extension
         _: Date.now() // prevent caching
-      }
+      };
 
       $.getJSON("/images/upload/cache/presign", options, function(result) {
         console.log("presign", result);
-        data.formData = result['fields'];
-        data.url = result['url'];
+        data.formData = result.fields;
+        data.url = result.url;
         data.paramName = "file";
-        data.submit()
+        data.submit();
       });
 
     },
@@ -22,7 +22,7 @@ $(document).on("turbolinks:load", function() {
       console.log("progress", data);
 
       var progress = parseInt(data.loaded / data.total * 100, 10);
-      var percentage = progress.toString() + '%'
+      var percentage = progress.toString() + '%';
       data.progressBar.find(".progress-bar").css("width", percentage).html(percentage);
     },
     done: function(e, data) {
@@ -38,11 +38,11 @@ $(document).on("turbolinks:load", function() {
           filename: data.files[0].name.match(/[^\/\\]+$/)[0], // IE return full path
           mime_type: data.files[0].type
         }
-      }
+      };
 
-      form = $(this).closest("form");
-      form_data = new FormData(form[0]);
-      form_data.append($(this).attr("name"), JSON.stringify(image))
+      var form = $(this).closest("form");
+      var form_data = new FormData(form[0]);
+      form_data.append($(this).attr("name"), JSON.stringify(image));
 
       $.ajax(form.attr("action"), {
         contentType: false,
